ad1bd821a8322ee006b230790d65ff4b29e7d284
Anna Karachkovska <akarachkovska@gmail.com>
2022-10-04 22:29:48 +0300

    New update

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Notiflix from 'notiflix';
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+// Notiflix.Notify.warning('Memento te hominem esse');
+// Notiflix.Notify.info('Cogito ergo sum');
+
+// const axios = require('axios');
+// async function getUser() {
+//     try {
+//       const response = await axios.get('/user?ID=12345');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
